f658dae89c16d7fe23edd74026466288ce58d2cc
saqo-torosyan <s.torosyan83@gmail.com>
2022-04-19 18:02:22 +0400

    making schedule

--- model/schedule.js
+++ model/schedule.js
@@ -1,5 +1,19 @@
 const mongoose =require('mongoose')
 
 const scheduleSchema = mongoose.Schema({
-  
+  startDate: {
+    type: Date,
+    required: true
+  },
+
+  endDate: {
+    type: Date,
+    required: true
+  },
+
+  custom: {
+    type: Number,
+    required: true
+  }
+
 })
