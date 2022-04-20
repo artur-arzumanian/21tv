ff88670a636e74aeb096b0cf32802f4c59d22df4
saqo-torosyan <s.torosyan83@gmail.com>
2022-04-20 13:14:43 +0400

    auth change

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,39 +6,38 @@ require('dotenv').config()
 const auth = async (req, res, next) => {
   try{
     const token = await req.header('Authorization').replace('Bearer ', '')
-
+    console.log(token)
     if (!token) {
       return res.status(403).send("A token is required for authentication");
-    } else {
-        let dateNow = new Date();
+    }else{
+      let dateNow = new Date();
 
-        const parseJwt = (token) => {
-         try {
-           return JSON.parse(atob(token.split('.')[1]));
-         } catch (e) {
-           return null;
-         }
-        };
-
-        const decodedJwt = parseJwt(admin.token)
-
-        const admin = await Admin.findOne({_id: decodedJwt._id})
-        if(!admin){
-          throw new Error()
+      const parseJwt = (token) => {
+        try {
+          return JSON.parse(atob(token.split('.')[1]));
+        } catch (e) {
+          return null;
         }
+      };
 
-        if (admin.isAdmin === false) {
-          return res.status(403).send({error: 'You haven\'t permission'})
-        }
+      const decodedJwt = parseJwt(token)
+      const admin = await Admin.findOne({_id: decodedJwt._id})
 
-        if (decodedJwt.exp < Math.floor(dateNow.getTime()/1000)){
-           return res.status(403).send({error: 'Token is expired. Please login again'})
-        } else {
-          jwt.verify(token, process.env.JWT_PRIVATE_KEY)
-          req.token = token
-          req.admin = admin
-          next()
-        }
+      if(!admin){
+        throw new Error()
+      }
+
+      if (admin.isAdmin === false) {
+        return res.status(403).send({error: 'You haven\'t permission'})
+      }
+      if (decodedJwt.exp < Math.floor(dateNow.getTime()/1000)){
+        return res.status(403).send({error: 'Token is expired. Please login again'})
+      }else {
+        jwt.verify(token, process.env.JWT_PRIVATE_KEY)
+        req.token = token
+        req.admin = admin
+        next()
+      }
     }    
   }catch(e){
     res.status(403).send({error: 'Please authenticate!'})
