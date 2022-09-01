33e5048db14e23a2da2018abb1e61486c5e3c191
saqo-torosyan <s.torosyan83@gmail.com>
2022-09-01 15:36:33 +0400

    mailchanges

--- controller/contact_us.js
+++ controller/contact_us.js
@@ -57,7 +57,6 @@ exports.sendEmailTo21TV = async (req,res) => {
  
   const {google} = require('googleapis')
   const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
-  console.log(oAuth2Client);
   oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
 
   const accessToken = await new Promise((resolve, reject) => {
@@ -69,75 +68,82 @@ exports.sendEmailTo21TV = async (req,res) => {
       });
   });
 
-  var readHTMLFile = function(path, callback) {
-    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
-        if (err) {
-          callback(err); 
-          throw err;
-            
-        }
-        else {
-          callback(null, html);
-        }
-    });
-  };
-
-  smtpTransport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.AUTH_USER,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.REFRESH_TOKEN,
-      accessToken: accessToken
-    },
-    tls: {rejectUnauthorized: false}
-  });
-
-  readHTMLFile(__dirname + '/../emailTamplate/emailTamplate.html', function(err, html) {
-  
-    var template = handlebars.compile(html);
-    var replacements = {
-      lastName,
-      firstName,
-      email,
-      phone,
-      text
+  if(accessToken){
+    var readHTMLFile = function(path, callback) {
+      fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+          if (err) {
+            callback(err); 
+            throw err;
+              
+          }
+          else {
+            callback(null, html);
+          }
+      });
     };
-    var htmlToSend = template(replacements);
-    var mailOptions = {
-      to: "21tv.development@gmail.com",
-      subject: "Contact us",
-      attachments: [
-        {
-          filename: 'email.png',
-          path: __dirname +'/../public/email_img/email.png',
-          cid: 'emailImg'
-        },
-        {
-          filename: 'phone.png',
-          path: __dirname +'/../public/email_img/phone.png',
-          cid: 'phoneImg'
-        },
+
+    smtpTransport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.AUTH_USER,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken
+      },
+      tls: {rejectUnauthorized: false}
+    });
+
+    readHTMLFile(__dirname + '/../emailTamplate/emailTamplate.html', function(err, html) {
       
-        {
-          filename: 'logo.png',
-          path:  __dirname + '/../public/email_img/logo.png',
-          cid: 'logoImg'
-        }
-      ],
-      html: htmlToSend
-    }; 
-    
-    smtpTransport.sendMail(mailOptions, function (error, response) {
-      if (error) {
-        
-        res.status(500).send({msg: "Email hasn't been sent", error})
-      }else{
-        res.send("Email has been sent");
+      if(err){
+        return res.send({msg: "Invalid path", error})
       }
+      var template = handlebars.compile(html);
+      var replacements = {
+        lastName,
+        firstName,
+        email,
+        phone,
+        text
+      };
+      var htmlToSend = template(replacements);
+      var mailOptions = {
+        to: "21tv.development@gmail.com",
+        subject: "Contact us",
+        attachments: [
+          {
+            filename: 'email.png',
+            path: __dirname +'/../public/email_img/email.png',
+            cid: 'emailImg'
+          },
+          {
+            filename: 'phone.png',
+            path: __dirname +'/../public/email_img/phone.png',
+            cid: 'phoneImg'
+          },
+        
+          {
+            filename: 'logo.png',
+            path:  __dirname + '/../public/email_img/logo.png',
+            cid: 'logoImg'
+          }
+        ],
+        html: htmlToSend
+      }; 
+      
+      smtpTransport.sendMail(mailOptions, function (error, response) {
+        if (error) {        
+          res.status(500).send({msg: "Email hasn't been sent", error})
+        }else{
+          res.status(500).send({msg: "Email has been sent", response});
+        }
+      }); 
     }); 
-  }); 
+
+  }else{
+    return res.status(500).send({msg: "Invalid access token"});
+  }
  
 }
